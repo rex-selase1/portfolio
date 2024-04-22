0764b3d89032f8c1cecf19d8a6001f9e9388bdad
rex-selase1 <rexselasetaylor@gmail.com>
2024-04-22 07:42:03 -0600

    "done"

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,22 +1,23 @@
 import "./Skills.css";
 // import { useState, useEffect } from 'react';
-import { projects } from '../../assets/assets';
-import { assets } from '../../assets/assets';
+// import { projects } from "../../assets/assets";
+import { assets } from "../../assets/assets";
 
 const Skills = () => {
+  console.log(assets[0]);
   return (
     <div id="skills" className="skills">
       <h1>Skills</h1>
       <div className="cards">
         <div className="card1">
-          <img src={assets.html} alt="html" />
-          <img src={assets.css} alt="css" />
-          <img src={assets.javascript} alt="javascript" />
+          <img src={assets[0]} alt="html" />
+          <img src={assets[1]} alt="css" />
+          <img src={assets[2]} alt="javascript" />
         </div>
         <div className="card2">
-          <img src={assets.nodeJs} alt="" />
-          <img src={assets.mongo} alt="" />
-          <img src={assets.react} alt="" />
+          <img src={assets[3]} alt="" />
+          <img src={assets[4]} alt="" />
+          <img src={assets[5]} alt="" />
         </div>
       </div>
     </div>
