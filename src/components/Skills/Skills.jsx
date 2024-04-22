22edbc9b1a832b8d1b77ed89b579e7b007346a5d
rex-selase1 <rexselasetaylor@gmail.com>
2024-04-22 05:26:12 -0600

    "trial"

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -7,14 +7,14 @@ const Skills = () => {
       <h1>Skills</h1>
       <div className="cards">
         <div className="card1">
-          <img src="./public/images/html.jpg" alt="html" />
-          <img src="./public/images/css.png" alt="css" />
-          <img src="./public/images/JavaScript-logo.png" alt="javascript" />
+          <img src="/images/html.jpg" alt="html" />
+          <img src="images/css.png" alt="css" />
+          <img src="/images/JavaScript-Logo.png" alt="javascript" />
         </div>
         <div className="card2">
-          <img src="public/images/react.jpg" alt="" />
-          <img src="public/images/nodejs.png" alt="" />
-          <img src="public/images/mongo.jpg" alt="" />
+          <img src="/images/react.jpg" alt="" />
+          <img src="/images/nodejs.png" alt="" />
+          <img src="/images/mongo.jpg" alt="" />
         </div>
       </div>
     </div>
